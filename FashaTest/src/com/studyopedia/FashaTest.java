408a2d786c0a990487e2aedd150c76093487f0f8
User <User@LAPTOP-QC69QVEV>
2023-10-16 00:32:06 +0800

    NURRINA FASSHA

---
+++ FashaTest/src/com/studyopedia/FashaTest.java
@@ -0,0 +1,11 @@
+package com.studyopedia;
+
+public class FashaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi, my name is Fasha");
+	}
+
+}
